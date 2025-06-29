0be269b29bc0fa82fefe755b7d66e742dc1786bc
Denys Orlyk <idsorlyk@gmail.com>
2025-06-29 09:23:26 +0200

    Add smooth scrolling, update header styles, and enhance modal functionality
    - Implement smooth scrolling behavior in animations.css.
    - Modify header styles in header.css for improved layout and responsiveness.
    - Include animations.css in styles.css for better organization.
    - Update logo.svg to use symbol for better accessibility and performance.
    - Enhance modal functionality in main.js to close on menu link click.
    - Update header.html to reference the new logo symbol and improve structure.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-menu-open]"),
-      closeModalBtn: document.querySelector("[data-menu-close]"),
-      modal: document.querySelector("[data-menu]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
+  const refs = {
+    openModalBtn: document.querySelector("[data-menu-open]"),
+    closeModalBtn: document.querySelector("[data-menu-close]"),
+    modal: document.querySelector("[data-menu]"),
+  };
 
-      refs.modal.classList.toggle("is-open");
-    }
-  })();
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-open");
+  }
+
+  
+  const menuLinks = document.querySelectorAll('.modal-header-menu-list a');
+
+  menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      refs.modal.classList.remove('is-open');
+    });
+  });
+})();
